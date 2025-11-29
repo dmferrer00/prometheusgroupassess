9e10e9143c6991282578fb4c80306b24dfc49841
Daniel Ferrer <danielferrer@Daniels-Mac-mini.local>
2025-11-29 16:41:15 -0500

    feat/5 tests, setup, config

---
+++ PostAPI.Tests/PostsTests.cs
@@ -0,0 +1,96 @@
+﻿using NUnit.Framework;
+using RestSharp;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System;
+using System.Text.Json.Nodes; 
+
+namespace PostAPI.Tests;
+
+public class Tests
+{
+    private RestClient _client;
+    private IConfiguration _config;
+
+    [SetUp]
+    public void Setup()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+
+        _config = builder.Build();
+
+        var baseUrl = _config["BaseUrl"];
+        _client = new RestClient(new RestClientOptions(baseUrl));
+    }
+
+    [Test]
+    public void getPostsTest()
+    {
+        var request = new RestRequest("/posts/1", Method.Get);
+        var response = _client.Execute(request);
+
+        Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+        Assert.That(response.Content, Is.Not.Null.And.Not.Empty);
+
+        var post = JsonNode.Parse(response.Content);
+        Assert.That(post, Is.Not.Null);
+        Assert.That(post["id"]?.GetValue<int>(), Is.EqualTo(1));
+    }
+
+    [Test]
+    public void newPostsTest()
+    {
+        var request = new RestRequest("/posts", Method.Post);
+        var postData = new { title = "Associate QA", body = "Test body content", userId = 5 };
+        request.AddJsonBody(postData);
+        var response = _client.Execute(request);
+
+        Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.Created));
+        Assert.That(response.Content, Is.Not.Null.And.Not.Empty);
+
+        var createdPost = JsonNode.Parse(response.Content);
+        Assert.That(createdPost, Is.Not.Null);
+        Assert.That(createdPost["title"]?.GetValue<string>(), Is.EqualTo("Associate QA"));
+    }
+
+    [Test]
+    public void changePosts()
+    {
+        var request = new RestRequest("/posts/1", Method.Put);
+        var postData = new { id = 1, title = "SDET", body = "The body has been updated.", userId = 1 };
+        request.AddJsonBody(postData);
+        var response = _client.Execute(request);
+
+        Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+        Assert.That(response.Content, Is.Not.Null.And.Not.Empty);
+
+        var updatedPost = JsonNode.Parse(response.Content);
+        Assert.That(updatedPost, Is.Not.Null);
+        Assert.That(updatedPost["title"]?.GetValue<string>(), Is.EqualTo("SDET"));
+    }
+
+    [Test] 
+    public void deletePostsTest()
+    {
+        var request = new RestRequest("/posts/1", Method.Delete);
+        var response = _client.Execute(request);
+        
+        Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+    }
+
+    [Test]
+    public void getNegativePostsTest()
+    {
+        var request = new RestRequest("/posts/-1", Method.Get);
+        var response = _client.Execute(request);
+        Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.NotFound));
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _client?.Dispose();
+    }
+}
--- PostAPI.Tests/obj/Debug/net10.0/PostAPI.Tests.AssemblyInfo.cs
+++ PostAPI.Tests/obj/Debug/net10.0/PostAPI.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PostAPI.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a343f0bbd36bd7dbfcc05a98087a1d67e7c1435")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+077bdc4fd7b3cf3c0b5095c0374808f14d9979a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PostAPI.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PostAPI.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
